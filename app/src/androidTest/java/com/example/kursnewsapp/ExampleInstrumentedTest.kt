24a1a6700c00f310f3c8c7ecfca25e8c52721086
Milana46 <bogatyrevamilana462004@gmail.com>
2024-11-26 11:48:48 +0300

    test of domain

--- app/src/androidTest/java/com/example/kursnewsapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/kursnewsapp/ExampleInstrumentedTest.kt
@@ -1,23 +1,15 @@
 package com.example.kursnewsapp
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.kursnewsapp.data.api.RetrofitInstance
+
 import com.example.kursnewsapp.data.api.newsAPI
+import com.example.kursnewsapp.domain.Article
+import com.example.kursnewsapp.domain.Source
 import com.example.kursnewsapp.presentation.util.Constants
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-
-
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -28,9 +20,9 @@ class NewsApiTest {
 
     @Before
     fun setUp() {
-        val client=OkHttpClient.Builder().build()
+        val client = OkHttpClient.Builder().build()
 
-        val ret= Retrofit.Builder()
+        val ret = Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
@@ -39,20 +31,161 @@ class NewsApiTest {
         val api by lazy {
             ret.create(newsAPI::class.java)
         }
-        Api=api
+        Api = api
 
     }
 
     @Test
     fun testGetHeadlines_Success() = runBlocking {
-       assert(Api.getHeadlines().isSuccessful)
+        assert(Api.getHeadlines().isSuccessful)
 
     }
 
-
-
-
 }
+
+class ArticleUnitTest {
+
+    @Test
+    fun testDataOfArticles_Success() = runBlocking {
+        val expectedArticles = listOf(
+            Article(
+                key = 1,
+                author = "Author1",
+                content = "Content1",
+                description = "Description1",
+                publishedAt = "2024-11-26",
+                source = Source(id = "1", name = "Source1"),
+                title = "Title1",
+                url = "https://url1.com",
+                urlToImage = "https://image1.com"
+            ),
+            Article(
+                key = 2,
+                author = "Author2",
+                content = "Content2",
+                description = "Description2",
+                publishedAt = "2024-11-26",
+                source = Source(id = "2", name = "Source2"),
+                title = "Title2",
+                url = "https://url2.com",
+                urlToImage = "https://image2.com"
+            ),
+            Article(
+                key = 3,
+                author = "Author3",
+                content = "Content3",
+                description = "Description3",
+                publishedAt = "2024-11-26",
+                source = Source(id = "3", name = "Source3"),
+                title = "Title3",
+                url = "https://url3.com",
+                urlToImage = "https://image3.com"
+            ),
+            Article(
+                key = 4,
+                author = "Author4",
+                content = "Content4",
+                description = "Description4",
+                publishedAt = "2024-11-26",
+                source = Source(id = "4", name = "Source4"),
+                title = "Title4",
+                url = "https://url4.com",
+                urlToImage = "https://image4.com"
+            ),
+            Article(
+                key = 5,
+                author = "Author5",
+                content = "Content5",
+                description = "Description5",
+                publishedAt = "2024-11-26",
+                source = Source(id = "5", name = "Source5"),
+                title = "Title5",
+                url = "https://url5.com",
+                urlToImage = "https://image5.com"
+            )
+        )
+
+        val actualArticles = listOf(
+            Article(
+                key = 1,
+                author = "Author1",
+                content = "Content1",
+                description = "Description1",
+                publishedAt = "2024-11-26",
+                source = Source(id = "1", name = "Source1"),
+                title = "Title1",
+                url = "https://url1.com",
+                urlToImage = "https://image1.com"
+            ),
+            Article(
+                key = 2,
+                author = "Author2",
+                content = "Content2",
+                description = "Description2",
+                publishedAt = "2024-11-26",
+                source = Source(id = "2", name = "Source2"),
+                title = "Title2",
+                url = "https://url2.com",
+                urlToImage = "https://image2.com"
+            ),
+            Article(
+                key = 3,
+                author = "Author3",
+                content = "Content3",
+                description = "Description3",
+                publishedAt = "2024-11-26",
+                source = Source(id = "3", name = "Source3"),
+                title = "Title3",
+                url = "https://url3.com",
+                urlToImage = "https://image3.com"
+            ),
+            Article(
+                key = 4,
+                author = "Author4",
+                content = "Content4",
+                description = "Description4",
+                publishedAt = "2024-11-26",
+                source = Source(id = "4", name = "Source4"),
+                title = "Title4",
+                url = "https://url4.com",
+                urlToImage = "https://image4.com"
+            ),Article(
+                key = 5,
+                author = "Author5",
+                content = "Content5",
+                description = "Description5",
+                publishedAt = "2024-11-26",
+                source = Source(id = "5", name = "Source5"),
+                title = "Title5",
+                url = "https://url5.com",
+                urlToImage = "https://image5.com"
+            )
+        )
+
+        assertEquals(expectedArticles.size, actualArticles.size)
+
+
+        for (i in expectedArticles.indices) {
+            val expected = expectedArticles[i]
+            val actual = actualArticles[i]
+
+            assertEquals("Key mismatch at index $i", expected.key, actual.key)
+            assertEquals("Author mismatch at index $i", expected.author, actual.author)
+            assertEquals("Content mismatch at index $i", expected.content, actual.content)
+            assertEquals("Description mismatch at index $i", expected.description, actual.description)
+            assertEquals("PublishedAt mismatch at index $i", expected.publishedAt, actual.publishedAt)
+            assertEquals("Source ID mismatch at index $i", expected.source.id, actual.source.id)
+            assertEquals("Source Name mismatch at index $i", expected.source.name, actual.source.name)
+            assertEquals("Title mismatch at index $i", expected.title, actual.title)
+            assertEquals("URL mismatch at index $i", expected.url, actual.url)
+            assertEquals("URLToImage mismatch at index $i", expected.urlToImage, actual.urlToImage)
+        }
+    }
+}
+
+
+
+
 
 //@RunWith(AndroidJUnit4::class)
 //class ExampleInstrumentedTest {
